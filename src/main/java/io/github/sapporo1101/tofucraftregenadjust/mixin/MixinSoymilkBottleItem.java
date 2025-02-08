96cbfe33c01b623469197ce1d078457aba69e127
sapporo1101 <71534849+sapporo1101@users.noreply.github.com>
2025-02-08 13:33:30 +0900

    adjust effect duration

--- src/main/java/io/github/sapporo1101/tofucraftregenadjust/mixin/MixinSoymilkBottleItem.java
+++ src/main/java/io/github/sapporo1101/tofucraftregenadjust/mixin/MixinSoymilkBottleItem.java
@@ -1,6 +1,7 @@
 package io.github.sapporo1101.tofucraftregenadjust.mixin;
 
 import baguchi.tofucraft.item.SoymilkBottleItem;
+import baguchi.tofucraft.registry.TofuEffects;
 import net.minecraft.core.Holder;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -12,12 +13,14 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 @Mixin(value = SoymilkBottleItem.class)
 public class MixinSoymilkBottleItem {
 
-    @ModifyArg(method = "finishUsingItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;addEffect(Lnet/minecraft/world/effect/MobEffectInstance;)Z", ordinal = 2), index = 0)
+    @ModifyArg(method = "finishUsingItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;addEffect(Lnet/minecraft/world/effect/MobEffectInstance;)Z"), index = 0)
     private MobEffectInstance modifyMobEffectInstance(MobEffectInstance mobEffectInstance) {
         final Holder<MobEffect> mobEffect = mobEffectInstance.getEffect();
         int duration = mobEffectInstance.getDuration();
         if (mobEffect == MobEffects.REGENERATION) {
-            duration = (int) (Math.log(duration / 20.0) / Math.log(1.5) * 20);
+            duration = (int) (Math.log(duration / 20.0 * 2.5) * 20);
+        } else if (mobEffect == TofuEffects.SOY_HEALTHY) {
+            duration = (int) (Math.log(duration / 20.0 * 10) * 20);
         }
         return new MobEffectInstance(mobEffect, duration, mobEffectInstance.getAmplifier());
     }
